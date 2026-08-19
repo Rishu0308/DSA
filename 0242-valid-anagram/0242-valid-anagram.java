78f291943b6f4098bc575af109739ed9ddaac5e6
Rishu Kumar <136003059+Rishu0308@users.noreply.github.com>
2026-08-20 03:40:58 +0530

    Time: 4 ms (83.06%), Space: 44.3 MB (74.99%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        int freq[] = new int[26];
+
+        for(int i = 0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            freq[ch - 'a'] = freq[ch - 'a'] + 1;
+        }
+
+        for(int i = 0; i<t.length(); i++){
+            char ch = t.charAt(i);
+            freq[ch - 'a'] = freq[ch - 'a'] - 1;
+        }
+
+        for(int i = 0; i<26; i++){
+            if(freq[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
